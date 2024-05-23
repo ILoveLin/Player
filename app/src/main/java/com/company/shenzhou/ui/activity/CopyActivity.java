b74ed484babd2f27346b3c6a506a1da0368bb81c
tongzhengtong <tongzhengtong@126.com>
2024-05-23 09:07:26 +0800

    cmeplayer重构版本

---
+++ app/src/main/java/com/company/shenzhou/ui/activity/CopyActivity.java
@@ -0,0 +1,28 @@
+package com.company.shenzhou.ui.activity;
+
+import com.company.shenzhou.app.AppActivity;
+import com.company.shenzhou.R;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 可进行拷贝的副本
+ */
+public final class CopyActivity extends AppActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.copy_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
