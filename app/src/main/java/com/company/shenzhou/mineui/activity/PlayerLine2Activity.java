39ba10c0b71b4a50adab9b45a91fe272be755459
tongzhengtong <tongzhengtong@126.com>
2024-07-08 15:35:41 +0800

    up
    1，删除vlc

--- app/src/main/java/com/company/shenzhou/mineui/activity/PlayerLine2Activity.java
+++ app/src/main/java/com/company/shenzhou/mineui/activity/PlayerLine2Activity.java
@@ -6,7 +6,6 @@ import android.widget.RelativeLayout;
 import com.company.shenzhou.R;
 import com.company.shenzhou.app.AppActivity;
 import com.company.shenzhou.utlis.LogUtils;
-import com.company.shenzhou.widget.vlc.MyControlVlcVideoView;
 
 /**
  * company: 江西神州医疗设备有限公司
@@ -18,7 +17,6 @@ public final class PlayerLine2Activity extends AppActivity implements View.OnCli
     private static final String TAG = "线路2，界面==";
     public static String mPath01 = "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8";
 
-    private MyControlVlcVideoView mControlPlayView;
     private View.OnTouchListener onTouchVideoListener;
     private RelativeLayout mRootView;
 
@@ -30,94 +28,7 @@ public final class PlayerLine2Activity extends AppActivity implements View.OnCli
 
     @Override
     protected void initView() {
-        mControlPlayView = findViewById(R.id.player_control);
-        onTouchVideoListener = mControlPlayView.getOnTouchVideoListener();
-        //触摸控制亮度和声音,是否可触摸开关
-        mRootView = mControlPlayView.getRootView();
-        mRootView.setLongClickable(true);  //手势需要--能触摸
-        mRootView.setOnTouchListener(onTouchVideoListener);
 
-        mControlPlayView.setPlayerTitle("你好，我是播放器--1");
-        mControlPlayView.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //开始播放
-                mControlPlayView.setStartLive(mPath01);
-
-            }
-        }, 500);
-
-        mControlPlayView.setOnPlayerOrClickListener(new MyControlVlcVideoView.onPlayerOrClickListener() {
-            @Override
-            public void eventPlay() {
-                LogUtils.e(TAG + "eventPlay");
-                MyControlVlcVideoView.onPlayerOrClickListener.super.eventPlay();
-            }
-
-            @Override
-            public void eventStop() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.eventStop();
-                LogUtils.e(TAG + "eventStop");
-
-            }
-
-            @Override
-            public void eventError() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.eventError();
-                LogUtils.e(TAG + "eventError");
-
-            }
-
-            @Override
-            public void onClickBack() {
-                finish();
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickBack();
-                LogUtils.e(TAG + "onClickBack");
-
-            }
-
-            @Override
-            public void onClickSetting() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickSetting();
-                LogUtils.e(TAG + "onClickSetting");
-
-            }
-
-            @Override
-            public void onClickChangeLive() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickChangeLive();
-                LogUtils.e(TAG + "onClickChangeLive");
-
-            }
-
-            @Override
-            public void onClickMic() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickMic();
-                LogUtils.e(TAG + "onClickMic");
-
-            }
-
-            @Override
-            public void onClickPhotos() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickPhotos();
-                LogUtils.e(TAG + "onClickPhotos");
-
-            }
-
-            @Override
-            public void onClickSnapShot() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickSnapShot();
-                LogUtils.e(TAG + "onClickSnapShot");
-
-            }
-
-            @Override
-            public void onClickRestart() {
-                MyControlVlcVideoView.onPlayerOrClickListener.super.onClickRestart();
-                LogUtils.e(TAG + "onClickRestart");
-
-            }
-        });
     }
 
     @Override
